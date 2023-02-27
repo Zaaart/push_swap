a06ffa467780e2696295d529d6575a66215fca1e
dylan <dumontdylan11pro@gmail.comclear>
2023-02-27 14:29:29 +0100

    debut

---
+++ Push Swap/push_swap.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dydumont <dydumont@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/27 14:12:04 by dydumont          #+#    #+#             */
+/*   Updated: 2023/02/27 14:27:36 by dydumont         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+
+#endif
